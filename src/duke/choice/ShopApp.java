23909fe710666bd7237b0e430516164807aece74
angelapaganucci <angelapaganucci@hotmail.com>
2021-06-07 22:45:20 -0300

    Add Constructor

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -8,31 +8,14 @@ public class ShopApp {
 
         System.out.println("Welcome to Duck Choice Shop");
 
-        Customer c1 = new Customer();
+        Customer c1 = new Customer("Pinky", 14);
 
-        c1.setName("Pinky");
-        c1.setSize("S");
+        //c1.setSize("S");
 
-        Clothing item1 = new Clothing();
-        Clothing item2 = new Clothing();
+        Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
+        Clothing item2 = new Clothing("Orange T-Shirt",10.5, "S");
 
-        Clothing[] items = {item1, item2, new Clothing(), new Clothing()};
-
-        item1.setDescription("Blue Jacket");
-        item1.setPrice(20.9);
-        item1.setSize("M");
-
-        item2.setDescription("Orange T-Shirt");
-        item2.setPrice(10.5);
-        item2.setSize("S");
-
-        items[2].setDescription("Green Scarf");
-        items[2].setPrice(5.0);
-        items[2].setSize("S");
-
-        items[3].setDescription("Blue T-Shirt");
-        items[3].setPrice(10.5);
-        items[3].setSize("S");
+        Clothing[] items = {item1, item2, new Clothing("Green Scarf", 5.0, "S" ), new Clothing("Blue T-Shirt", 10.5, "S")};
 
 //       System.out.println("Item 1, " + item1.description + ", " + item1.price + ", " + item1.size);
 //       System.out.println("Item 2, " + item2.description + ", " + item2.price + ", " + item2.size);
@@ -41,7 +24,7 @@ public class ShopApp {
 
         c1.addItems(items);
 
-        c1.setSize(measurement);
+        //c1.setSize(measurement);
         System.out.println("Customer is " + c1.getName() + ", " + c1.getSize() + ", " + c1.getTotalClothingCost());
 
         for (Clothing item : c1.getItems()){
