ed820ee88f097e3698643bb0596991a3301c0de9
angelapaganucci <angelapaganucci@hotmail.com>
2021-06-08 14:16:10 -0300

    Try_catch

--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -12,10 +12,12 @@ public class ShopApp {
 
         //c1.setSize("S");
 
-        Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
-        Clothing item2 = new Clothing("Orange T-Shirt",10.5, "S");
+        System.out.println("Min Price " + Clothing.MIN_PRICE);
 
-        Clothing[] items = {item1, item2, new Clothing("Green Scarf", 5.0, "S" ), new Clothing("Blue T-Shirt", 10.5, "S")};
+        Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
+        Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
+
+        Clothing[] items = {item1, item2, new Clothing("Green Scarf", 5.0, "S"), new Clothing("Blue T-Shirt", 10.5, "S")};
 
 //       System.out.println("Item 1, " + item1.description + ", " + item1.price + ", " + item1.size);
 //       System.out.println("Item 2, " + item2.description + ", " + item2.price + ", " + item2.size);
@@ -27,8 +29,25 @@ public class ShopApp {
         //c1.setSize(measurement);
         System.out.println("Customer is " + c1.getName() + ", " + c1.getSize() + ", " + c1.getTotalClothingCost());
 
-        for (Clothing item : c1.getItems()){
+        for (Clothing item : c1.getItems()) {
             System.out.println("Items " + item.getDescription());
+        }
+
+        int average = 0;
+        int count = 0;
+
+        for (Clothing item : c1.getItems()) {
+            if (item.getSize().equals("L")) {
+                count++;
+                average += item.getPrice();
+            }
+        }
+        try {
+            average = (count == 0) ? 0 : average/count;
+            average = average / count;
+            System.out.println("Average price " + average + ", Count " + count);
+        } catch (ArithmeticException e){
+            System.out.println("Don't divide by 0");
         }
     }
 }
